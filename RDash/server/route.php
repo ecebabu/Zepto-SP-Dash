983dd11be8fa423d5d9ac6297432efbb0728b270
XIOR <xiortechnology@gmail.com>
2026-02-09 00:22:34 +0530

    Fix local dev routing for static files

--- RDash/server/route.php
+++ RDash/server/route.php
@@ -1,6 +1,13 @@
 <?php
 
 // Load configuration first
+if (php_sapi_name() == 'cli-server') {
+    $file = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if (is_file($file)) {
+        return false;
+    }
+}
+
 require_once __DIR__ . '/config.php';
 
 // CORS Headers - Support multiple origins
